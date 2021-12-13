d07691ee2f05cbe904fd5290f27ff194c35e3dd9
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2021-12-13 19:49:24 +0800

    Update blbl.js

--- blbl.js
+++ blbl.js
@@ -4,7 +4,7 @@
 
 [rewrite_local]
 #哔哩哔哩解锁大会员
-^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/weigege008/Quantumultx-weiren/main/blbl.js
+^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/QuantumultX-weiren/main/blbl.js
 
 [mitm]
 hostname = *.biliapi.*, *.bilibili.*
