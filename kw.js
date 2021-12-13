f85d4cd2799891e45f069dc6bf7d05ab78a9d242
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2021-12-13 22:51:24 +0800

    Add files via upload

---
+++ kw.js
@@ -0,0 +1,42 @@
+/*
+
+作者伟人q55749353
+
+Quantumult X 脚本:
+
+[rewrite_local]
+
+#酷我音乐🎵
+^https?:\/\/musicpay\.kuwo\.cn\/music\.pay\?uid\=\d+ url 302 http://musicpay.kuwo.cn/music.pay?uid=2
+^https?:\/\/.*kuwo.*(/AdService/kaiping/adinfo|/vip/v2/user/vip).*$ url script-response-body kw.js
+
+MITM = vip1.kuwo.cn
+*/
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const p1 = '/AdService/kaiping/';
+const p2 = '/vip/v2/user/vip';
+
+//开屏广告
+ if (url.indexOf(p1) != -1) {
+	obj = {};
+	body = JSON.stringify(obj);
+}
+//会员修改
+ if (url.indexOf(p2) != -1) {	
+	obj.data.luxAutoPayUser = "2";
+	obj.data.vipLuxuryExpire = "32493834549000";	
+	obj.data.isYearUser = "2";
+	obj.data.vipmExpire = "32493834549000";
+	obj.data.vipOverSeasExpire = "32493834549000";
+	obj.data.vipExpire = "32493834549000";
+	obj.data.vip3Expire = "32493834549000";
+	obj.data.experienceExpire = "32493834549000"
+body = JSON.stringify(obj);
+}
+
+$done({body});
+
+
